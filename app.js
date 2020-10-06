be86e942c8c32d426591d12a234cddd07568fb6f
= <=>
2020-10-06 14:05:04 +0700

    10/6

--- app.js
+++ app.js
@@ -5,27 +5,13 @@ const cors = require('cors');
 const mongodb = require('mongodb');
 const { json } = require('body-parser');
 
-let MongoClient = mongodb.MongoClient;
-let url = 'mongodb://localhost:27017/Fishing';
+const config = require('./constant/config')
+
+const MongoClient = mongodb.MongoClient;
+
 const app = express();
 app.use(express.static("view"));
-const port = 3000;
 
-// Where we will keep books
-let products =[];
-
-MongoClient.connect(url, function (err, db) {
-    if (err) throw err;
-    var a = db.collection('product');
-    a.find({}).toArray(function (err,data) {
-        //nếu lỗi
-        if (err) throw err;
-        //nếu thành công
-        console.log(data);
-        products=data;
-    });
-    db.close();
-});
 
 app.use(cors());
 
@@ -43,12 +29,31 @@ const readHTML=(path,res)=>{
 
 app.get('/products', (req, res) => {
     setTimeout(a=>{
-        res.json(products);
+        let products =[];
+        MongoClient.connect(config.DATABASE_URL, function (err, db) {
+            if (err) {
+                console.log("Connect thất bại")
+                throw err;
+            }
+            else
+            {
+                let product = db.collection('product');
+                product.find({}).toArray(function (err,data) {
+                    //nếu lỗi
+                    if (err) throw err;
+                    //nếu thành công
+                    console.log("Connect thành công")
+                    products=data;
+                    res.json(products);
+                });
+                db.close();
+            }
+        });
     },2000);
 });
 
 app.get('/', (req, res) => {
-    readHTML('./view/home.html',res)
+    readHTML(config.homePath,res)
 });
 
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
+app.listen(config.port, () => console.log(`Hello world app listening on port ${config.port}!`));
---
+++ constant/config.js
@@ -0,0 +1,9 @@
+const DATABASE_URL = 'mongodb://localhost:27017/Fishing';
+const port = 3000;
+const homePath='./view/home.html';
+
+module.exports={
+    DATABASE_URL: DATABASE_URL,
+    port: port,
+    homePath: homePath,
+};
